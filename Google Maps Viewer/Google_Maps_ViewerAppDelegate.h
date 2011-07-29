b79bbbb0866f2e1a753f9210bd4ce8308679c378
Sheyne <me@sheyne.com>
2011-07-29 13:11:00 -0600

    First Commit after reset

---
+++ Google Maps Viewer/Google_Maps_ViewerAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  Google_Maps_ViewerAppDelegate.h
+//  Google Maps Viewer
+//
+//  Created by Sheyne Anderson on 7/27/11.
+//  Copyright 2011 Sheyne Anderson. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Google_Maps_ViewerAppDelegate : NSObject <NSApplicationDelegate> {
+	NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
